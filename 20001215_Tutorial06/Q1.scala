2d3d41060459e8e9439c84f72ffe2ada72c2715c
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2022-09-06 05:40:49 +0530

    Add files via upload

---
+++ 20001215_Tutorial06/Q1.scala
@@ -0,0 +1,28 @@
+object CaesarCipher extends App {
+    import scala.io.StdIn._
+
+    val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    print("Enter a string: ")
+    val str = readLine()
+
+    print("Shift by: ")
+    val shift = readInt()
+
+    val encrypt=(c:Char,key:Int,a:String)=>
+        a((a.indexOf(c.toUpper)+key)%a.size)
+
+    val decrypt=(c:Char,key:Int,a:String)=>
+        a((a.indexOf(c.toUpper)-key)%a.size)
+
+    val cipher = (algo:(Char, Int, String) =>
+    Char, s:String, key:Int, a:String) =>
+    s.map(algo(_, key, a))
+
+    val ct = cipher(encrypt, str, shift, alphabet)
+
+    val pt = cipher(decrypt, ct, shift, alphabet)
+
+    println("\nEncrypted: " + ct)
+    println("Decrypted: " + pt)
+}
